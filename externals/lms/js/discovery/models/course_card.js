80265161a58a5183dbaead06c31042686122a337
root <root@ginko1rc1.localdomain>
2017-08-11 09:40:09 -0700

    ficus.base

---
+++ externals/lms/js/discovery/models/course_card.js
@@ -0,0 +1,25 @@
+(function(define) {
+    define(['backbone'], function(Backbone) {
+        'use strict';
+
+        return Backbone.Model.extend({
+            defaults: {
+                modes: [],
+                course: '',
+                enrollment_start: '',
+                number: '',
+                content: {
+                    overview: '',
+                    display_name: '',
+                    number: ''
+                },
+                start: '',
+                image_url: '',
+                org: '',
+                id: '',
+                effort: '',
+                advertised_start: ''
+            }
+        });
+    });
+})(define || RequireJS.define);
